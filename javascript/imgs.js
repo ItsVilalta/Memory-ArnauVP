ea97bdfed4d77f493960a68c9953819fd00b49cf
Arnau VP <92567683+ItsVilalta@users.noreply.github.com>
2024-03-28 12:21:12 +0100

    RoutesJS2

--- javascript/imgs.js
+++ javascript/imgs.js
@@ -3,16 +3,16 @@
 // a cada parella per poder saber quines parelles son.
 
 var icones = [
-    "<img src='../img/vscode_icon.png' value='1'>", "<img src='../img/vscode_icon.png' value='1'>",
-    "<img src='../img/discord.png' value='2'>", "<img src='../img/discord.png' value='2'>",
-    "<img src='../img/apple.png' value='3'>", "<img src='../img/apple.png' value='3'>",
-    "<img src='../img/wasap.png' value='4'>", "<img src='../img/wasap.png' value='4'>",
-    "<img src='../img/Youtube_logo.png' value='5'>", "<img src='../img/Youtube_logo.png' value='5'>",
-    "<img src='../img/js_logo.png' value='7'>", "<img src='../img/js_logo.png' value='7'>",
-    "<img src='../img/instagram.png' value='6'>", "<img src='../img/instagram.png' value='6'>",
-    "<img src='../img/php.png' value='8'>", "<img src='../img/php.png' value='8'>",
-    "<img src='../img/tuiter.png' value='9'>", "<img src='../img/tuiter.png' value='9'>",
-    "<img src='../img/spotify.png' value='10'>", "<img src='../img/spotify.png' value='10'>",
-    "<img src='../img/applemusic.png' value='11'>", "<img src='../img/applemusic.png' value='11'>",
-    "<img src='../img/settings.png' value='12'>", "<img src='../img/settings.png' value='12'>",
+    "<img src='./../img/vscode_icon.png' value='1'>", "<img src='./../img/vscode_icon.png' value='1'>",
+    "<img src='./../img/discord.png' value='2'>", "<img src='./../img/discord.png' value='2'>",
+    "<img src='./../img/apple.png' value='3'>", "<img src='./../img/apple.png' value='3'>",
+    "<img src='./../img/wasap.png' value='4'>", "<img src='./../img/wasap.png' value='4'>",
+    "<img src='./../img/Youtube_logo.png' value='5'>", "<img src='./../img/Youtube_logo.png' value='5'>",
+    "<img src='./../img/js_logo.png' value='7'>", "<img src='./../img/js_logo.png' value='7'>",
+    "<img src='./../img/instagram.png' value='6'>", "<img src='./../img/instagram.png' value='6'>",
+    "<img src='./../img/php.png' value='8'>", "<img src='./../img/php.png' value='8'>",
+    "<img src='./../img/tuiter.png' value='9'>", "<img src='./../img/tuiter.png' value='9'>",
+    "<img src='./../img/spotify.png' value='10'>", "<img src='./../img/spotify.png' value='10'>",
+    "<img src='./../img/applemusic.png' value='11'>", "<img src='./../img/applemusic.png' value='11'>",
+    "<img src='./../img/settings.png' value='12'>", "<img src='./../img/settings.png' value='12'>",
 ];
